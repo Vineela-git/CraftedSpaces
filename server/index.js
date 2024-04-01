4214c18036086e784928c6f735c7f3dc5c48887f
Vineela Madduri <vineela.madduri@gmail.com>
2024-03-31 21:46:40 -0500

    resolved conflicts

--- server/index.js
+++ server/index.js
@@ -2,51 +2,67 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const mongoose = require("mongoose");
-const userdetailsModel = require ("./models/userdetails.js");
+const userdetailsModel = require("./models/userdetails.js");
+const professionalModel = require("./models/professionalDetails.js");
 port = 3001;
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose.connect("mongodb://127.0.0.1:27017/userdetails", )
+mongoose.connect("mongodb://127.0.0.1:27017/userdetails");
 
 app.post("/signup", async (req, res) => {
-
-  // Store a user-copy on Chat Engine!
-  // Docs at rest.chatengine.io
+  // Store a user/professional
+  
   try {
-    userdetailsModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
+    const userData = req.body;
+    if(userData.company){
+      professionalModel.create(userData)
+      .then((professionals)=> res.json(professionals))
+      .catch((err)=>res.json(err))
+    }else{
+      userdetailsModel
+      .create(req.body)
+      .then((users) => res.json(users))
+      .catch((err) => res.json(err));
+    }
+    
   } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
 });
-app.post("/login", async(req,res)=>{
-  try{
-const {email, password}= req.body;
-userdetailsModel.findOne({email: email})
-.then(user => {
-  if(user){
-    if(user.password === password){
-      res.json("success")
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    userdetailsModel.findOne({ email: email }).then((user) => {
+      if (user) {
+        if (user.password === password) {
+          res.json("usersuccess");
+        } else {
+          res.json("invalid password");
+        }
+      } else {
+        professionalModel.findOne({ email: email }).then((professional)=>{
+          if (professional) {
+            if (professional.password === password) {
+              res.json("success");
+            } else {
+              res.json("invalid password");
+            }
+          }else{
+            res.json("No records found, Please sign up first!")
+          }
+        })
     }
-    else{
-      res.json("wrong pass")
-    }
+  })
+  } catch {
+    console.log("error trying to login", 401);
   }
-  else{
-    res.json("not found")
-  }
-})
-}catch{
-  console.log("error trying to login", 401);
-}
-})
+});
 
-app.get('/', function (req, res) {
-  res.render('index', {});
+app.get("/", function (req, res) {
+  res.render("index", {});
 });
 // vvv On port 3001!
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
--- src/component/contact/FormOne.js
+++ src/component/contact/FormOne.js
@@ -1,96 +1,104 @@
-import React, { useRef, useState} from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import UserLoginHome from '../../pages/UserLoginHome';
 
 const FormOne = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const [loggedIn, setLoggedIn] = useState(false);
-    
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    const form = useRef();
+  const form = useRef();
 
-    const [ result, showresult ] = useState(false);
-    const[email, setEmail] = useState();
-    const[password, setPassword] = useState();
-    const [errorMessage, setErrorMessage] = useState('');
+  const [result, showresult] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  //prevents the default form submission behavior, which is a page reload
+  //Once you've prevented the default behavior, you can execute custom logic
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-   
-//prevents the default form submission behavior, which is a page reload
-//Once you've prevented the default behavior, you can execute custom logic
+    await axios
+      .post("http://localhost:3001/login", { email, password })
+      .then((result) => {
+        console.log(result);
+        if (result.data === "usersuccess") {
+          navigate("/userhome");
+        } else if (result.data === "success") {
+          navigate("/");
+        } else if (result.data === "invalid password") {
+          setErrorMessage("invalid password");
+        }
+        else{
+            setErrorMessage("No records found, Please sign up first!")
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
+  setTimeout(() => {
+    showresult(false);
+  }, 5000);
 
-      const handleLogin = async(e) => {
-        e.preventDefault();
-        
-await axios.post("http://localhost:3001/login", {email,password})
-.then(result => {console.log(result)
-    if(result.data==="success"){
-navigate("/userhome")}
-else if(result.data==="wrong pass"){
-    setErrorMessage("Invalid password");
-}
-else{
-    setErrorMessage("User not found");  
-}})
-.catch(err => console.log(err),
-// setErrorMessage("An error occurred while logging in. Please try again later.")
-);
-        // Normally would call API and validate credentials
-        // For now, we're just simulating login
-       
-      }
-        
+  //To navigate to signup page when the button gets clicked.
 
-        setTimeout(() => {
-            showresult(false);
-        }, 5000);
+  const handleSignup = () => {
+    navigate("/signup");
+  };
 
-//To navigate to signup page when the button gets clicked. 
-
-        const handleSignup = () => {
-            navigate('/signup');
-          }
-       
-
-          if(!loggedIn) {
-          
+  if (!loggedIn) {
     return (
-
-      
-        <form ref={form} onSubmit={handleLogin}  className="axil-contact-form">
-        
+      <form ref={form} onSubmit={handleLogin} className="axil-contact-form">
         <div id="login" className="form-group">
-            <label>Email</label>
-            <input type="email" className="form-control" name="contact-email" placeholder="example@mail.com" onChange={(e)=>setEmail(e.target.value)} required />
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            name="contact-email"
+            placeholder="example@mail.com"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div className="form-group mb--40">
-            <label>Password</label>
-            <input type="password" className="form-control" name="password" placeholder="........" onChange={(e)=>setPassword(e.target.value)} required />
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="........"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {errorMessage && <div className="error"> {errorMessage} </div>}
         </div>
-        
-        <div className="form-group mb--40"><label><a href="#">Forgot Password?</a></label>
+
+        <div className="form-group mb--40">
+          <label>
+            <a href="#">Forgot Password?</a>
+          </label>
         </div>
 
         <div className="form-group">
-            <button type="login" className="axil-btn btn-fill-primary btn-fluid btn-primary" name="login-btn">Login</button>
-            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-
+          <button
+            type="login"
+            className="axil-btn btn-fill-primary btn-fluid btn-primary"
+            name="login-btn"
+          >
+            Login
+          </button>
         </div>
-    
-        <div className="form-group"> Don't have an account? <a href="/signup">Sign up here</a> </div>
-        
-      
-    </form>
-    ) 
 
-    
-          }
-          
-}
+        <div className="form-group">
+          {" "}
+          Don't have an account? <a href="/signup">Sign up here</a>{" "}
+        </div>
+      </form>
+    );
+  }
+};
 
 export default FormOne;
